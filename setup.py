6c2b4f6c8b286d93f18e6e89119d1ad698305d4c
Joseph Joice <joseph_joice@intuit.com>
2017-10-17 23:31:38 -0700

    setup fix

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     author="Joseph Joice",
     author_email="joseph_joice@intuit.com",
     description="AWS ssh through bastion",
-    package_dir={'': 'sshthru'},
-    packages=find_packages("sshthru")
+    packages=['sshthru']
 )
